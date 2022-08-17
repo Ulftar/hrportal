7ed670ec82097d4bf77ff8490ac42616c3652758
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-08-17 17:01:28 +0700

    Add project create and update form

--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,22 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     return render(request, 'single-project.html', {'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('projects')
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
